2ab2ab3aa51efc2c8a2a65750a6cf29bdeec18fd
John William Jones III <johnwilliamjonesiii@gmail.com>
2023-02-28 00:17:13 -0600

    Recreated project in Visual Studio 2022, with updated SDK's for .Net

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ASP.NetProjectAppMKII.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
